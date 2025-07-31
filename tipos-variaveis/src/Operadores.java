a4f08553d1c312e3952731726ea0542f25f3af74
Leandro <leandroinacio@gmail.com>
2025-07-31 19:14:33 -0300

    Adiciona novos arquivos e pastas

---
+++ tipos-variaveis/src/Operadores.java
@@ -0,0 +1,19 @@
+public class Operadores {
+    public static void main (String[] args){
+        String nomeUm = "Leandro";
+        String nomeDois = new String("Leandro");
+
+        System.out.println(nomeUm.equals(nomeDois));
+
+
+
+
+        int numero1 = 1;
+        int numero2 = 2;
+        
+        boolean simNao = numero1 != numero2;
+
+        System.out.println("numeroUm é diferente a numeroDois? " + simNao);
+        //teste
+    }
+}
